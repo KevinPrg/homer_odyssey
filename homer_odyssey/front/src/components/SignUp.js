beb759e4b0b6c650b6c8dc0dd9b9f8e3dccd98cd
psycho33200 <pininfarina47@gmail.com>
2019-12-30 11:00:20 +0100

    formu quete 002

--- homer_odyssey/front/src/components/SignUp.js
+++ homer_odyssey/front/src/components/SignUp.js
@@ -1,22 +1,70 @@
 import React, { Component } from "react";
+import "../style/signup.css";
 
 export class SignUp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: "TEST"
+      email: "",
+      password: "",
+      passwordBis: "",
+      name: "",
+      lastname: ""
     };
   }
 
   updateEmailField = e => {
-    this.setState({ email: e.target.value });
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(JSON.stringify(this.state, 1, 1));
   };
 
   render() {
     return (
-      <div>
-        <h1>{this.state.email}</h1>
-        <input type="email" name="email" onChange={this.updateEmailField} />
+      <div className="container">
+        <h1>{JSON.stringify(this.state, 1, 1)}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="contain">
+            email:
+            <input type="email" name="email" onChange={this.updateEmailField} />
+          </div>
+          <div className="contain">
+            password:
+            <input
+              type="password"
+              name="password"
+              onChange={this.updateEmailField}
+            />
+          </div>
+          <div className="contain">
+            repeat password:
+            <input
+              type="password"
+              name="passwordBis"
+              onChange={this.updateEmailField}
+            />
+          </div>
+          <div className="contain">
+            name:
+            <input type="text" name="name" onChange={this.updateEmailField} />
+          </div>
+          <div className="contain">
+            lastname:
+            <input
+              type="text"
+              name="lastname"
+              onChange={this.updateEmailField}
+            />
+          </div>
+          <div className="contain">
+            <input type="submit" value="Soumettre" />
+          </div>
+        </form>
       </div>
     );
   }
